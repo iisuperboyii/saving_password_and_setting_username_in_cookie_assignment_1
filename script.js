ab02c1046c914746d9925a3b7fc64e6ef84236e5
iiuperboyii <aaronjomon24@gmail.com>
2025-03-11 09:21:10 +0530

    completed assignment

--- script.js
+++ script.js
@@ -15,9 +15,15 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // 1. Get the value of the 'count' cookie
+  let count = getCookie('count')
+
   // 2. If the cookie exists, increment the value and update the cookie
+  count = count ? parseInt(count)+1 : 1;
+
+  setCookie('count',count,7)
   // 3. If the cookie does not exist, create it and set the value to 1
   // 4. Display the count on the webpage
+  document.body.innerHTML += `<p>Page View Count: ${count}</p>`
 
   // your code here
 });
